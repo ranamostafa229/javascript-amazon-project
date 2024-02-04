22f5546ba1335ae576d7bcf5f48fb17b90634a95
rana mostafa <you@example.com>
2024-02-05 00:41:16 +0200

    Make select quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -24,7 +24,7 @@ products.forEach((product) => {
             </div>
 
             <div class="product-quantity-container">
-            <select>
+            <select class="js-quantity-selector-${product.id}">
                 <option selected value="1">1</option>
                 <option value="2">2</option>
                 <option value="3">3</option>
@@ -59,30 +59,35 @@ document.querySelector(".js-products-grid").innerHTML = productHTML;
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
-
     let matchingItem;
+
+    const quantitySelector = document.querySelector(
+      `.js-quantity-selector-${productId}`
+    );
+
+    const quantity = Number(quantitySelector.value);
+
     cart.forEach((item) => {
       if (productId === item.productId) {
         matchingItem = item;
       }
     });
     if (matchingItem) {
-      matchingItem.quantity += 1;
+      matchingItem.quantity += quantity;
     } else {
       cart.push({
         productId: productId,
-        quantity: 1,
+        quantity: quantity,
       });
     }
 
     let cartQuantity = 0;
 
     cart.forEach((item) => {
-      cartQuantity += item.quantity;
+      cartQuantity = cartQuantity + item.quantity;
     });
 
     document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
-
     // button.innerHTML = "Added";
   });
 });
